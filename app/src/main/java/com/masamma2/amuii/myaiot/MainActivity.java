4acb54df1d6629575688bff2ec4b7242f25bfbe8
amuii <masamma2@inf.upv.es>
2015-02-27 19:31:56 +0100

    Hilos en java funcionando

--- app/src/main/java/com/masamma2/amuii/myaiot/MainActivity.java
+++ app/src/main/java/com/masamma2/amuii/myaiot/MainActivity.java
@@ -1,19 +1,67 @@
 package com.masamma2.amuii.myaiot;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+
+    synchronized void imprimir(final String cad) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                TextView ll = (TextView) findViewById(R.id.TextViewEstado);
+                ll.append(cad);
+                ScrollView ll1 = (ScrollView) findViewById(R.id.scrollView);
+                ll1.fullScroll(View.FOCUS_DOWN);
+            }});
+    }
+
+    void imprimirml(String cad){
+        imprimir(cad+"\r\n");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+
+    /*hilo de registro*/
+        new Thread(new Runnable() {
+            public void run() {
+                imprimir("Hilo de Registro");
+                // inicio flowcloud
+                // registro
+                // obtener nombre
+            }
+        }
+        ).start();
+
+        new Thread(new Runnable() {
+            public void run() {
+                imprimir("Hilo de prueba");
+                // inicio flowcloud
+                // registro
+                // obtener nombre
+                while(true){
+                    try {
+                        imprimir("Hilo de prueba,dentro del while");
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+        ).start();
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
